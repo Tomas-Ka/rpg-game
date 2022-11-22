8263cbd1630a7251f3a00aed7fb362321f21d467
Tomas-Ka <tomas.kamsater@gmail.com>
2022-11-22 10:14:24 +0100

    added player movement

--- code/player.py
+++ code/player.py
@@ -3,6 +3,34 @@ from settings import *
 
 class Player(pygame.sprite.Sprite):
     def __init__(self, pos, groups):
-        super.__init__(groups)
-        self.image = pygame.image.load('../graphics/test/rock.png').convert_alpha()
+        super().__init__(groups)
+        self.image = pygame.image.load('../graphics/test/player.png').convert_alpha()
         self.rect = self.image.get_rect(topleft = pos)
+        
+        self.direction = pygame.math.Vector2()
+        self.speed = 5
+    
+    def input(self):
+        keys = pygame.key.get_pressed()
+        
+        self.direction.x = 0
+        self.direction.y = 0
+        
+        if keys[pygame.K_UP]:
+            self.direction.y -= 1
+        if keys[pygame.K_DOWN]:
+            self.direction.y += 1
+            
+        if keys[pygame.K_LEFT]:
+            self.direction.x -= 1
+        if keys[pygame.K_RIGHT]:
+            self.direction.x += 1
+    
+    def move(self, speed):
+        if self.direction.magnitude() != 0:
+            self.direction = self.direction.normalize()
+        self.rect.center += self.direction * speed
+
+    def update(self):
+        self.input()
+        self.move(self.speed)
