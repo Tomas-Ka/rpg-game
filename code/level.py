0025e33105be49a07f6d1abecaf4a8d5b59bfa63
Tomas-Ka <tomas.kamsater@gmail.com>
2022-11-23 12:51:13 +0100

    Added a camera that follows the player

--- code/level.py
+++ code/level.py
@@ -10,7 +10,7 @@ class Level():
         self.display_surface = pygame.display.get_surface()
         
         # sprite group setup
-        self.visible_sprites = pygame.sprite.Group()
+        self.visible_sprites = YsortCameraGroup()
         self.obstacle_sprites = pygame.sprite.Group()
 
         self.create_map()
@@ -28,6 +28,24 @@ class Level():
     
     def run(self):
         # update and draw the game
-        self.visible_sprites.draw(self.display_surface)
+        self.visible_sprites.custom_draw(self.player)
         self.visible_sprites.update()
-        debug(self.player.direction)
+        
+
+class YsortCameraGroup(pygame.sprite.Group):
+    def __init__(self):
+        super().__init__()
+        self.display_surface = pygame.display.get_surface()
+        self.half_width = self.display_surface.get_size()[0] // 2
+        self.half_height = self.display_surface.get_size()[1] // 2
+        self.offset = pygame.math.Vector2()
+    
+    def custom_draw(self, player):
+        
+        # get offset
+        self.offset.x = player.rect.centerx - self.half_width
+        self.offset.y = player.rect.centery - self.half_height
+        
+        for sprite in self.sprites():
+            offset_rect = sprite.rect.topleft - self.offset
+            self.display_surface.blit(sprite.image, offset_rect)
